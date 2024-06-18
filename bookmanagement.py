2b33d6ccc81fd3647adebb0987503851e696f900
jomaway <jomaway@user.noreply.github.com>
2024-06-18 14:50:52 +0200

    feat: add book_list function

--- bookmanagement.py
+++ bookmanagement.py
@@ -5,8 +5,13 @@ def book_add(inventory, title, author, price):
     new_book = {
         'title': title,
         'author': author,
-        'preis': price,
+        'price': price,
     }
     inventory.append(new_book)
 
-
+# Bucher auflisten
+def book_list(inventory):
+    for book in inventory:
+            print(f"Titel: {book['title']}, Autor: {book['author']}, Preis: {book['price']}")
+           
+           
