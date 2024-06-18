9fcb383b42fc4b9e4add8b03b5428f70f071ef19
jomaway <jomaway@user.noreply.github.com>
2024-06-18 15:00:02 +0200

    feat: add book_search function

--- bookmanagement.py
+++ bookmanagement.py
@@ -13,5 +13,12 @@ def book_add(inventory, title, author, price):
 def book_list(inventory):
     for book in inventory:
             print(f"Titel: {book['title']}, Autor: {book['author']}, Preis: {book['price']}")
-           
-           
+                        
+# Buch suchen
+def book_search(inventor, title):
+    for book in inventory:
+        if book['title'].lower() == title.lower():
+            return book 
+    return None
+
+
